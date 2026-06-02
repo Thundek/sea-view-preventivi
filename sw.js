80f329c4d74534af8a3dd3280290b18ef2989dda
Thundek <salvo.monaco.cri@gmail.com>
2026-06-02 17:20:01 +0200

    Initial commit: Hotel Quote PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'hotel-preventivi-v1';
+const ASSETS = [
+  './preventivo-hotel.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
